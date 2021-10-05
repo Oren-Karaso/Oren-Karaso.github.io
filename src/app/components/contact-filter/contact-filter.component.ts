b99de9e304f4d5d167dec8cdc1af3cd9caf1656e
Oren Karaso <orenkaraso@gmail.com>
2021-10-05 13:22:48 +0300

    filter-css

--- src/app/components/contact-filter/contact-filter.component.ts
+++ src/app/components/contact-filter/contact-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact-filter',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFilterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
   }
